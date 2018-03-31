2cc6557ab8b0fd353a84c11693ff455f57722997
Alex Page <alex@alexpage.com.au>
2018-03-31 12:15:26 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,134 @@
 🖼 Harmonograph
 ==============
 
-> Draw beautiful geometric shapes in the browser
+> Draw beautiful randomised lissajous curves in the browser
+
+
+## Contents
+
+* [Install](#install)
+* [How to use](#use)
+* [Settings](#settings)
+* [Tests](#tests)
+* [Release History](#release-history)
+
+
+## Install
+
+```shell
+npm install harmonograph
+```
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## How to use
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+	<canvas id="harmonograph" width="800" height="800">
+		This only works with modern browsers that <a href="https://caniuse.com/#feat=canvas"></a>support the `canvas`</a> element.
+	</canvas>
+	<script src="node_modules/harmonograph/harmonograph.min.js" ></script>
+	<script>Harmonograph({ element: document.getElementById( "harmonograph" ) });</script>
+</body>
+</html>
+```
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Settings
+
+You can change the settings by adding values to the Harmonograph function. For example:
+
+```js
+Harmonograph({
+	element: document.getElementById( "harmonograph" ),
+	color: '#fff',
+	speed: 20,
+	drawingTime: 300,
+	pendulum: [{
+		amplitude: 200, frequency: 2.985, phase: 2.054, damping: 0.001
+	},
+	{
+		amplitude: 200, frequency: 3.006, phase: 1.820, damping: 0.008
+	},
+	{
+		amplitude: 200, frequency: 3.003, phase: 2.283, damping: 0.001
+	},
+	{
+		amplitude: 200, frequency: 1.994, phase: 1.155, damping: 0.001
+	}]
+});
+```
+
+
+### `element`
+_(object)_
+
+The html5 canvas element for the harmograph to be drawn inside.
+
+
+### `speed`
+_(number)_
+default: `10`
+
+The speed at which the harmonograph is drawn adds new x and y coordinates.
+
+
+### `color`
+_(string)_
+default: `#000`
+
+The color of the harmonograph lines
+
+
+### `drawingTime`
+_(number)_
+default: `200`
+
+How long until drawing should stop. A drawingTime of 1 would draw for 1 second.
+
+
+### `pendulum`
+_(array)_
+default: Two random pendulums will be created
+
+Two pendulums require four items ( x, y and x, y ) in the array. Each item contains an object in the array. Each object must use the following keys:
+
+**Amplitude:** 
+How far a pendulum swings back and forth, must be from `0` - `360` degrees
+
+**Frequency:** 
+How fast a pendulum swings back and forth, for best results use decimal values around `2` and `3`
+
+**Phase:**     
+The rate that a pendulum loses its energy, or slows down, must be from `0` to `π`
+
+**Damping:**   
+The offset from the normal starting position of a pendulum, must be from `0` to `0.01`
+
+
+**[⬆ back to top](#contents)**
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+## Release History
+
+* v0.1.0  - 💥 Initial version
+
+
+**[⬆ back to top](#contents)**
 
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /***************************************************************************************************************************************************************
  *
- * index.js -
+ * index.js - Draw beautiful randomised lissajous curves in the browser
  *
- * FunctionName()
+ * RandomNumber   - Get a random number inside a maximum and minimum value
+ * RandomPendulum - Create a randomised pendulum swing
+ * Harmonograph   - Draw a harmonograph to html5 canvas
  *
  **************************************************************************************************************************************************************/
 
@@ -10,118 +12,127 @@
 'use strict';
 
 
-const GenerateRandomInteger = ( min, max ) => {
-  return min + Math.random() * ( max + 1 - min );
+/**
+ * RandomNumber - Get a random number inside a maximum and minimum value
+ *
+ * @param  {number}  max   - The maximum value
+ * @param  {number}  min   - The minimum value
+ * @param  {boolean} round - If the number should be rounded
+ *
+ * @return {number}        - The randomised number
+ */
+function RandomNumber( max, min, round ) {
+	min   = min   ? min   : 0;
+	round = round ? round : false;
+	if ( round ){
+		return Math.floor( Math.random() * ( max - min ) + min );
+	}
+  return Math.random() * ( max - min ) + min ;
 }
-
-const SETTINGS = {
-	speed: 1,
-	line: {
-		width: 0.2,
-		color: '#fff'
-	},
-	pendulum: [{
-		amplitude: GenerateRandomInteger( 0, 360 ),
-		frequency: ( 2 + Math.random() / 40 ) % 10,
-		phase: GenerateRandomInteger( 0, 1.5 ) % ( Math.PI * 2 ),
-		damping: 0.02,
-	},
-	{
-		amplitude: GenerateRandomInteger( 0, 360 ),
-		frequency: ( 2 + Math.random() / 40 ) % 10,
-		phase: GenerateRandomInteger( 0, 1.5 ) % ( Math.PI * 2 ),
-		damping: 0.0315,
-	},
-	{
-		amplitude: GenerateRandomInteger( 0, 360 ),
-		frequency: ( 2 + Math.random() / 40 ) % 10,
-		phase: GenerateRandomInteger( 0, 1.5 ) % ( Math.PI * 2 ),
-		damping: 0.02,
-	},
-	{
-		amplitude: GenerateRandomInteger( 0, 360 ),
-		frequency: ( 2 + Math.random() / 40 ) % 10,
-		phase: GenerateRandomInteger( 0, 1.5 ) % ( Math.PI * 2 ),
-		damping: 0.02,
-	}]
-};
 
 
 /**
- * Harmonograph
+ * RandomPendulum - Create a randomised pendulum swing
  *
- * @param  {object}  element  - The canvas element to have the harmonograph drawn inside
- * @param  {object}  settings - The settings that contains url and width
+ * Resources:
+ * - http://www.worldtreesoftware.com/harmonograph-intro.html
+ *
+ * @return {number} settings.amplitude  - How far a pendulum swings back and forth
+ * @return {number} settings.frequency  - How fast a pendulum swings back and forth
+ * @return {number} settings.phase      - The rate that a pendulum loses its energy, or slows down
+ * @return {number} settings.damping    - The offset from the normal starting position of a pendulum
  */
-const Harmonograph = ( element, settings = SETTINGS ) => {
-	if( element.getContext ) {
-		let context  = element.getContext( '2d' );
+function RandomPendulum() {
+	return {
+		amplitude: 200,
+		frequency: RandomNumber( 4, 2, true ) + RandomNumber( 0.02, -0.02 ),
+		phase:     RandomNumber( 0, Math.PI ),
+		damping:   RandomNumber( 0, 0.01 ),
+	}
+}
 
-		// Clear the canvas
+
+/**
+ * Harmonograph - Draw a harmonograph to html5 canvas
+ *
+ * Resources:
+ * - https://en.wikipedia.org/wiki/Harmonograph
+ * - https://aschinchon.wordpress.com/2014/10/13/beautiful-curves-the-harmonograph/
+ *
+ * @param  {object}  settings.element     - The canvas element to have the harmonograph drawn inside
+ * @param  {number}  settings.speed       - The speed of the drawing
+ * @param  {number}  settings.color       - The color of the harmonograph
+ * @param  {number}  settings.drawingTime - How long until drawing should stop
+ * @param  {array}   settings.pendulum    - The pendulum settings, see RandomPendulum
+ */
+function Harmonograph( settings ) {
+
+	// Check if it's possible to draw the harmonograph
+	if( settings.element.getContext ) {
+
+		// If the values don't exist set a default value
+		var speed       = settings.speed       ? settings.speed       : 10;
+		var width       = settings.width       ? settings.width       : 0.1;
+		var color       = settings.color       ? settings.color       : '#000';
+		var drawingTime = settings.drawingTime ? settings.drawingTime : 150;
+		var pendulum    = settings.pendulum    ? settings.pendulum : [
+			RandomPendulum(),
+			RandomPendulum(),
+			RandomPendulum(),
+			RandomPendulum(),
+		];
+
+		// Set global variables
+		var element       = settings.element;     // The html5 canvas element
+		var time          = 0;                    // Time used in the calculation of XY coordinate
+		var startTime     = new Date().getTime(); // The total time for drawing
+		var drawFrame     = 0.001;                // How often an XY coordinate is drawn
+		var timeIncrement = speed * drawFrame;    // The speed the XY coordinates are drawn
+
+		// Prepare the canvas for drawing
+		var context = element.getContext( '2d' );
 		context.clearRect( 0, 0, element.width, element.height );
 		context.save();
 
-		// Apply settings to canvas
+		// Start the context
+		context.restore();
 		context.beginPath();
-		// context.moveTo( element.width / 2, element.height / 2 )
-		context.lineWidth   = settings.line.width;
-		context.strokeStyle = settings.line.color;
+		context.stroke();
 
-		// As time goes by
-		// DrawHarmonograph( element, context, settings );
-		for ( let time = 0; time < 100; time += 0.01 ) {
-			// Calculate movement of each pendulum
-			const movement = settings.pendulum.map( p => {
-				return p.amplitude * Math.sin( p.frequency * time + Math.PI * p.phase ) * Math.exp( -p.damping * time );
+		// Draw a new x and y value based off the speed
+		var drawHarmonograph = setInterval( function() {
+
+			// Line settings for stroke
+			context.imageSmoothingEnabled = true;
+			context.lineWidth             = width;
+			context.strokeStyle           = color;
+
+			// Calculate the pendulum movement
+			var movement = pendulum.map( function( p ) {
+				return p.amplitude * Math.sin( p.frequency * time + p.phase ) * Math.exp( -p.damping * time );
 			})
 
 			// Apply the movement to x and y coordinates
-			const x = movement[ 0 ] + movement[ 1 ];
-			const y = movement[ 2 ] + movement[ 3 ];
+			var x = movement[ 0 ] + movement[ 1 ];
+			var y = movement[ 2 ] + movement[ 3 ];
 
+			// Draw a connected line at the next x and y point
 			context.lineTo( x + element.width / 2, y + element.height / 2 );
-			time += 0.01;
-		}
+			context.stroke();
 
-		context.stroke();
-		context.restore();
+			// Increase the time and total drawing time
+			time += timeIncrement;
 
+			// Check if we should stop drawing the harmonograph
+			if( new Date().getTime() - startTime >= drawingTime * 1000 ) {
+				clearInterval( drawHarmonograph );
+			}
+		}, drawFrame );
 	}
+
+	// Harmonograph uses context and is not supported in some browsers
 	else {
 		element.innerHtml( 'Harmonograph not supported in this browser.' );
 		console.error( 'Harmonograph not supported in this browser.' );
 	}
-}
-
-/**
- * The harmonograph movements can be described matematically with the following equations
- * https://web.archive.org/web/20100904003022/http://hernan.amiune.com/labs/harmonograph/animated-harmonograph.html
- *
- * x = A1 * sin( f1 * t + p1 ) * exp( -d1 * t ) + A2 * sin( f2 * t + p2 ) * exp( -d2 * t )
- * y = A3 * sin( f3 * t + p3 ) * exp( -d3 * t ) + A4 * sin( f4 * t + p4 ) * exp( -d4 * t )
- *
- * in which f represents frequency, p represent phase, A represent amplitude, d represents damping and t represents time.
- *
- * @param {*} settings
- */
-
-const DrawHarmonograph = ( element, context, settings, time = 0 ) => {
-
-	// if ( time >= 100 ) {
-	// 	return
-	// }
-
-	// // Calculate movement of each pendulum
-	// const movement = settings.pendulum.map( p => {
-	// 	return p.amplitude * Math.sin( p.frequency * time + Math.PI * p.phase ) * Math.exp( -p.damping * time );
-	// })
-
-	// // Apply the movement to x and y coordinates
-	// const x = movement[ 0 ] + movement[ 1 ];
-	// const y = movement[ 2 ] + movement[ 3 ];
-
-	// context.lineTo( x + element.width / 2, y + element.height / 2 );
-	// time += 0.01;
-
-	// window.requestAnimationFrame( DrawHarmonograph( element, context, settings, time ) );
 }
